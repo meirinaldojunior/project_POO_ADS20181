39632dfcf05c4807ff4f6ed525fcc439db8fe08e
Rafael Neves <sr.rafaelaugusto@gmail.com>
2018-05-14 23:57:05 -0300

    DAO Livro

---
+++ src/project_poo_ads20181/acessos/DAOLivro.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project_poo_ads20181.acessos;
+
+import java.util.ArrayList;
+import project_poo_ads20181.classes.Livro;
+import project_poo_ads20181.erro.ConexaoException;
+import project_poo_ads20181.erro.DAOException;
+
+/**
+ *
+ * @author Rafael Augusto
+ */
+public interface DAOLivro {
+    
+    public void inserir (Livro livro) throws ConexaoException, DAOException;
+    
+    public void alterar (Livro livro) throws ConexaoException, DAOException;
+    
+    public void excluir (Livro livro) throws ConexaoException, DAOException;
+    
+    public ArrayList<Livro> lista() throws ConexaoException, DAOException;
+    
+}
