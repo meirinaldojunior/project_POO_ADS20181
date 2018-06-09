71398e0d2849da0786aa6d95841e2bbc829571d0
valterapolinario <38543794+valterapolinario@users.noreply.github.com>
2018-06-09 13:00:27 -0300

    regra de negocio finalizada

--- src/project_poo_ads20181/negocio/RNAutor.java
+++ src/project_poo_ads20181/negocio/RNAutor.java
@@ -5,7 +5,11 @@
  */
 package project_poo_ads20181.negocio;
 
+import java.util.ArrayList;
+import project_poo_ads20181.acessos.DAOAutorImplementa;
 import project_poo_ads20181.classes.Autor;
+import project_poo_ads20181.erro.ConexaoException;
+import project_poo_ads20181.erro.DAOException;
 import project_poo_ads20181.erro.GeralException;
 
 /**
@@ -20,8 +24,8 @@ public class RNAutor {
      */
     public void validaAutor(Autor a)throws GeralException{
         validarAtributos(a);
-        if(a.getId()==null || a.getId()< 1){
-            throw new GeralException("Produto invalido!");
+     if(a.getId()==null || a.getId()<1){
+         throw new GeralException(" Autor invalido");
         }
     }
     /**
@@ -37,8 +41,75 @@ public class RNAutor {
             throw new GeralException("Nome invalido!");
         }
      }
+    public void verificaDuplicidadeNome(Autor a)throws GeralException, DAOException {
+    DAOAutorImplementa dai = new DAOAutorImplementa();
+    try{
+    if(dai.consultaNome(a.getNome())!= null){
+    throw new GeralException("Nome inválido");
+    }
+    }catch(ConexaoException e){
+            throw new GeralException("Contacte o ADM.");
+        }catch(DAOException e){
+            throw new GeralException("BUG.");
+    }
+           
+    }
+    public void inserir(Autor a)throws GeralException, DAOException{
+    DAOAutorImplementa dai = new DAOAutorImplementa();
+    try{
+    dai.inserir(a);
+    }catch(ConexaoException e){
+            throw new GeralException("Contacte o ADM.");
+        }catch(DAOException e){
+            throw new GeralException("BUG.");
+    }
+          }
+  public void alterar(Autor a) throws GeralException, DAOException{
+  DAOAutorImplementa dai = new DAOAutorImplementa();
+  try{
+  dai.alterar(a);
+  }catch(ConexaoException e){
+            throw new GeralException("Contacte o ADM.");
+        }catch(DAOException e){
+            throw new GeralException("BUG.");
+              } 
+        }
+  
+ public void excluir(Autor a) throws GeralException, DAOException, ConexaoException{
      
-}
+     DAOAutorImplementa dai = new DAOAutorImplementa();
+     try{
+         dai.excluir(a);
+     }catch(ConexaoException e){
+            throw new GeralException("Contacte o ADM.");
+        }catch(DAOException e){
+            throw new GeralException("BUG.");
+              } 
+ }
+ 
+public ArrayList<Autor>listar() throws ConexaoException, DAOException, GeralException{
+        DAOAutorImplementa dai = new DAOAutorImplementa();
+        try{
+            ArrayList<Autor> listar = dai.listar();
+            return listar;
+        }catch(ConexaoException e){
+            throw new GeralException("Contacte o ADM.");
+        }catch(DAOException e){
+            throw new GeralException("BUG.");
+        }
+    
+    }
+ public Autor buscaPorId (int id) throws ConexaoException, DAOException, GeralException{
+     DAOAutorImplementa dai = new DAOAutorImplementa();
+     try{
+         Autor buscaPorId = dai.buscaPorId(id);
+         return buscaPorId;
+     }catch(ConexaoException e){
+            throw new GeralException("Contacte o ADM.");
+        }catch(DAOException e){
+            throw new GeralException("BUG.");
+ }
+    
 
 
 
