2a66d1896e7b419a8ed7b51448daebeb548fbba3
Rafael Neves <sr.rafaelaugusto@gmail.com>
2018-06-10 17:26:17 -0300

    Packege Atualizado do Projeto vinculado a RN, Fachada e TL  do Objeto Livro

--- src/project_poo_ads20181/acessos/DAOLivroImplementa.java
+++ src/project_poo_ads20181/acessos/DAOLivroImplementa.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package project_poo_ads20181.acessos;
 
 import java.sql.Connection;
@@ -11,10 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import project_poo_ads20181.classes.Autor;
-import project_poo_ads20181.classes.Categoria;
 import project_poo_ads20181.classes.Livro;
 import project_poo_ads20181.erro.ConexaoException;
 import project_poo_ads20181.erro.DAOException;
@@ -27,6 +19,13 @@ import project_poo_ads20181.util.GerenciadorConexaoMySql;
  */
 public class DAOLivroImplementa implements DAOLivro{
 
+    
+    /**
+     * MÉTODO USADO PARA INSERIR O REGISTRO NA BASE DE DADOS.
+     * @param livro - OBJETO QUE SERÁ INSERIDO.
+     * @throws ConexaoException
+     * @throws DAOException 
+     */
     @Override
     public void inserir(Livro livro) throws ConexaoException, DAOException {
         GerenciadorConexao gc;
@@ -49,7 +48,12 @@ public class DAOLivroImplementa implements DAOLivro{
             gc.desconectar(c);
         }     
     }
-
+    /**
+     * MÉTODO USADO PARA ALTERAR O REGISTRO NA BASE DE DADOS.
+     * @param livro - OBJETO QUE SERA INSERIDO.
+     * @throws ConexaoException
+     * @throws DAOException 
+     */
     @Override
     public void alterar(Livro livro) throws ConexaoException, DAOException {
         
@@ -74,7 +78,12 @@ public class DAOLivroImplementa implements DAOLivro{
             gc.desconectar(c);
         } 
     }
-
+    /**
+     * MÉTODO USADO PARA EXCLUIR O REGISTRO NA BASE DE DADOS.
+     * @param livro - OBJETO A SER INSERIDO.
+     * @throws ConexaoException
+     * @throws DAOException 
+     */
     @Override
     public void excluir(Livro livro) throws ConexaoException, DAOException {
         
@@ -97,7 +106,12 @@ public class DAOLivroImplementa implements DAOLivro{
         } 
     
     }
-
+    /**
+     * MÉTODO USADO PARA LISTAR OS REGISTRO DA BASE DE DADOS.
+     * @return - RETORNA A LISTA DE DADOS.
+     * @throws ConexaoException
+     * @throws DAOException 
+     */
     @Override
     public ArrayList<Livro> lista() throws ConexaoException, DAOException {
         
@@ -114,6 +128,9 @@ public class DAOLivroImplementa implements DAOLivro{
               Livro  a = new Livro();
                 a.setIdLivro(rs.getInt("Id_livro"));
                 a.setNomeLivro(rs.getString("Nome_livro"));
+                a.getCat().setIdCategoria(rs.getInt("Id_Categoria"));
+                a.getAutor().setId(rs.getInt("Id_Autor")); 
+                
                 lista.add(a);
             }
              return lista;
@@ -124,12 +141,83 @@ public class DAOLivroImplementa implements DAOLivro{
             GerenciadorConexaoMySql.getInstancia().desconectar(c);
         } 
     
-       //método consulta por ID faltando
+       
     }
+    /**
+     * MÉTODO USADO PARA CONSULTAR O NOME REGISTRO NA BASE DE DADOS.
+     * @param livro - OBJETO A SER PESQUISADO NA BASE DE DADOS.
+     * @return - RETORNA O ID E O NOME DO LIVRO
+     * @throws DAOException
+     * @throws ConexaoException 
+     */
+    public Livro consultarNome (Livro livro) throws DAOException,ConexaoException{
+        
+        GerenciadorConexao gc;
+        gc = GerenciadorConexaoMySql.getInstancia();
+        Connection c = gc.conectar();
+        
+         Livro liv = null;
+        
+        String sql = "SELECT Id_livro, Nome_livro FROM Livro WHERE Nome_livro=?";
+        
+        PreparedStatement pstm;
+        try{
+            pstm = c.prepareStatement(sql);
+            pstm.setString(1, livro.getNomeLivro());
+            ResultSet rs = pstm.executeQuery();
+            
+            if(rs.next()){
+                liv = new Livro();
+                liv.setIdLivro(rs.getInt("Id_livro") );
+                liv.setNomeLivro(rs.getString("Nome_livro") );
+                
+            }
+            
+            return liv;
+            
+        }catch(SQLException e){
+            throw new DAOException();
+        }finally{
+            gc.desconectar(c);
+        }
+ 
+    }
+    /**
+     * METODO USADO PARA CHECAR SE O ID É EXISTENTE NA BASE DE DADOS!
+     * @param livro - OBJETO A SER CHECADO.
+     * @return - RETORNA TRUE, SE EXISTIR.
+     * @throws DAOException
+     * @throws ConexaoException 
+     */
     
     
+    public boolean checkID (Livro livro) throws DAOException,ConexaoException{
+        
+        GerenciadorConexao gc;
+        gc = GerenciadorConexaoMySql.getInstancia();
+        Connection c = gc.conectar();
+        
+        String sql = "SELECT Id_livro FROM Livro WHERE Id_livro=?";
+        
+        PreparedStatement pstm;
+        try{
+            pstm = c.prepareStatement(sql);
+            pstm.setInt(1, livro.getIdLivro());
+            ResultSet rs = pstm.executeQuery();
+            
+            while(rs.next()){
+                return true;
+            }
+            
+            return false;
+            
+        }catch(SQLException e){
+            throw new DAOException();
+        }finally{
+            gc.desconectar(c);
+        }
     
     
-    
+    }
     
 }
---
+++ src/project_poo_ads20181/fachada/FachadaLivro.java
@@ -0,0 +1,99 @@
+
+package project_poo_ads20181.fachada;
+
+import java.util.ArrayList;
+import project_poo_ads20181.acessos.DAOAutorImplementa;
+import project_poo_ads20181.acessos.DAOCategoriaImplementa;
+import project_poo_ads20181.acessos.DAOLivroImplementa;
+import project_poo_ads20181.classes.Livro;
+import project_poo_ads20181.erro.ConexaoException;
+import project_poo_ads20181.erro.DAOException;
+import project_poo_ads20181.erro.GeralException;
+import project_poo_ads20181.negocio.RNLivro;
+
+/**
+ *
+ * @author Rafael Augusto
+ */
+public class FachadaLivro {
+    RNLivro rn;
+    DAOLivroImplementa dao;
+    DAOAutorImplementa daoa;
+    DAOCategoriaImplementa daoc;
+    
+    /**
+     * CHAMA A VALIDAÇÃO DA REGRA DE NEGÓCIO LIVRO INSERIR.
+     * @param L - OBJETO A SER VÁLIDADO
+     * @return - RETORNA TRUE SE O DADO FOI INSERIDO NA BASE DE DADOS.
+     * @throws GeralException
+     * @throws ConexaoException
+     * @throws DAOException 
+     */
+    public boolean inserirRegistro(Livro L) throws GeralException, ConexaoException, DAOException{
+        
+        rn = new RNLivro();
+        dao = new DAOLivroImplementa();
+        daoa = new DAOAutorImplementa();
+        daoc = new DAOCategoriaImplementa();
+        
+        rn.validarAtributos(L);
+        rn.validarDuplicidadeNome(L);
+        daoa.checkID(L.getAutor());
+        daoc.checkID(L.getCat());
+        dao.inserir(L);
+        return true;
+    }
+    /**
+     * CHAMA A VALIDAÇÃO DA REGRA DE NEGÓCIO LIVRO ALTERAR.
+     * @param L - OBJETO A SER VÁLIDADO.
+     * @return - RETORNA TRUE SE O DADO FOI INSERIDO NA BASE DE DADOS.
+     * @throws GeralException
+     * @throws ConexaoException
+     * @throws DAOException 
+     */
+    public boolean alterarRegistro(Livro L) throws GeralException, ConexaoException, DAOException{
+        
+        rn = new RNLivro();
+        dao = new DAOLivroImplementa();
+        
+        rn.validarAtributos(L);
+        rn.validarDuplicidadeNome(L);
+        rn.validarID(L);
+        dao.alterar(L);
+        return true;
+    }
+    /**
+     * CHAMA A VALIDAÇÃO DA REGRA DE NEGÓCIO LIVRO EXCLUIR.
+     * @param L - OBJETO A SER VÁLIDADO.
+     * @return - RETORNA TRUE SE O DADO FOI INSERIDO NA BASE DE DADOS.
+     * @throws GeralException
+     * @throws ConexaoException
+     * @throws DAOException 
+     */
+    public boolean excluirRegistro(Livro L) throws GeralException, ConexaoException, DAOException{
+        
+        rn = new RNLivro();
+        dao = new DAOLivroImplementa();
+        
+        rn.validarID(L);
+        dao.excluir(L);
+        return true;
+    }
+    /**
+     * CHAMA A VALIDAÇÃO DA REGRA DE NEGÓCIO LIVRO LISTAR.
+     * @return - RETORNA A LISTA DA BASE DE DADOS.
+     * @throws GeralException
+     * @throws ConexaoException
+     * @throws DAOException 
+     */
+    public ArrayList<Livro> listarRegistro() throws GeralException, ConexaoException, DAOException{
+        
+        rn = new RNLivro();
+        
+        return rn.listarRegistro();
+    }
+    
+    
+    
+    
+}
